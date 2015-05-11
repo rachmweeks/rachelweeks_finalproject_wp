249dc6ad42f84d45e3db7eecd06028d9c6a2ba62
Rachel Weeks <rachelmweeks@gmail.com>
2015-05-10 19:46:10 -0500

    Inner Page Styles

--- wp-content/themes/policypeople/single-bio.php
+++ wp-content/themes/policypeople/single-bio.php
@@ -5,13 +5,13 @@ Template Name: Bio
 
 get_header(); ?>
 
-<h1>Author Bio</h1>
+<section class="internal">
+	<h1>Author Bio</h1>
+	<img src="<? the_field('bio_pic'); ?>" >
+	<h2><?php the_field('pp_name'); ?></h2>
+	<p><?php the_field('bio'); ?></p>
 
-<h2><?php the_field('pp_name'); ?></h2>
-
-<p><?php the_field('bio'); ?></p>
-
-<img src="<? the_field('bio_pic'); ?>" >
+</section>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
